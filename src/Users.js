cd2a908f63be56d79bd20b05cb4d20828f43deed
Brandon Turner <brandontheturner@gmail.com>
2019-04-01 20:46:41 -0500

    changes

--- src/Users.js
+++ src/Users.js
@@ -1,19 +1,53 @@
-import React, { Component, Fragment } from 'react'
-import { Button } from '@material-ui/core'
+import React, { Component, Fragment } from "react";
+import { Button } from "@material-ui/core";
+import { connect } from "react-redux";
+import axios from "axios";
 
 class Users extends Component {
+  getUsers = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(response => this.props.setUsers(response.data))
+      .catch(error => console.log(error));
+  };
+
   render() {
     return (
       <Fragment>
         <div>
           <h3>Press the button to get users and display them below</h3>
-          <Button onClick={this.getUsers} variant="contained" color="primary">Get Users</Button>
+          <Button onClick={this.getUsers} variant="contained" color="primary">
+            Get Users
+          </Button>
         </div>
         <div className="users-block">
+          {this.props.users.map(user => {
+            return (
+              <div key={user.name}>
+                name: {user.name}
+                userName: {user.userName} Email: {user.email} URL:{" "}
+                {user.website}
+              </div>
+            );
+          })}
         </div>
       </Fragment>
-    )
+    );
   }
 }
 
-export default Users
+const mapStateToProps = state => {
+  return {
+    users: state.users
+  };
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    setUsers: users => dispatch({ type: "SET_USERS", value: users })
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Users);
